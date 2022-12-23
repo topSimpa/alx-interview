541208951d4984b95a0c8fd0f41335b8bea5edca
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2022-12-23 05:30:55 +0100

    complete: pascal_triangle implementation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+"""
+defines a function that compute pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """Implements pascals triangle use for series expansion.
+    calculate the coefficient of the first nth pascal"""
+    coefs = []
+    if n <= 0:
+        return coefs
+    j = 0
+    while j < n:
+        new = []
+        i = 0
+        while i <= j:
+            if i == 0 or i == j:
+                new.append(1)
+            else:
+                new.append(coefs[j - 1][i - 1] + coefs[j - 1][i])
+            i += 1
+        coefs.append(new)
+        j += 1
+    return coef
