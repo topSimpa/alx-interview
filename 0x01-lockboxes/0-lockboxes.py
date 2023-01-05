b301e0302a5f79604ccef24de8bb11a7dfbcffe5
Mohammed Abdulmujeeb Simpa <abdulmujeebm@yahoo.com>
2023-01-05 23:19:25 +0100

    new solution for lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-"""solution to lockboxes"""
+"""lockboxes solution"""
+
 
 def canUnlockAll(boxes):
-    """return true if boxes can be unlocked"""
-    if not boxes:
-        return True
-    to_unlock = {0: 0}
-    for i in range(1, len(boxes)):
-        to_unlock[i] = None
-
-    for box in to_unlock.keys():
-        if to_unlock[box] is not None:
-            for key in boxes[box]:
-                to_unlock[key] = key
-                if key < box:
-                    for back in boxes[key]:
-                        to_unlock[back] = back
-    return (set(to_unlock.keys())).issubset(set(to_unlock.values()))
+    """return true if all boxes can be unlocked"""
+    s = {0}.union(set(boxes[0]))
+    for box in range(0, len(boxes)):
+        if box in s:
+            for i in boxes[box]:
+                s = s.union(set(boxes[i]))
+    return set(range(1, len(boxes))).issubset(s)
