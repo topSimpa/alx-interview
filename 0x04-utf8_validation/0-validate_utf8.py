84c4a7e9719df299f92d3d90b3896dce0d9d310e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-05-27 04:47:41 +0100

    refactor: accounted for big bits intergers and intergers with 0 start

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,13 +5,19 @@ Main file for utf8_validation
 
 
 def xtract(num):
+    # print(f'integer {num}')
     byte_a = bin(num)[2:]
+    # print(f'Extracting from {byte_a}')
+    if (len(byte_a) < 8):
+        byte_a = ((8 - len(byte_a)) * '0') + byte_a
+        # print(f'changed to {byte_a}')
     bits = ''
     for i in byte_a:
         if i == '0':
             bits += i
             break
         bits += i
+    # print(bits)
     return (bits)
 
 
@@ -20,12 +26,20 @@ def is_byte(num):
 
 
 def many_bytes(num):
+    # print(num)
     a_byte = bin(num)[2:]
+    # print(a_byte)
+    if (len(a_byte) < 8):
+        a_byte = ((8 - len(a_byte)) * '0') + a_byte
+        # print(a_byte)
+    elif len(a_byte) > 8:
+        return (-1)
     n_bytes = 0
     for i in a_byte:
         if i == '0':
             break
         n_bytes += 1
+    # print(n_bytes)
     return (n_bytes)
 
 
@@ -44,9 +58,12 @@ def validUTF8(data):
             continue
         else:
             n_bytes = many_bytes(data[i])
+            if (n_bytes > 3) or (n_bytes == -1):
+                return (False)
             # print(f"i am {n_bytes} bytes")
             # print("tried")
             if (i + n_bytes + 1) > len(data):
+                # print('short')
                 return (False)
             con_num = data[i: (i + n_bytes + 1)]
             j = 0
@@ -55,7 +72,9 @@ def validUTF8(data):
                 j_next = int(xtract(con_num[j + 1]), 2)
                 # print(j_cur, j_next)
                 if right_shift(j_cur) != j_next:
+                    # print('shifting error')
                     return (False)
                 j += 1
+                t = True
             i = i + n_bytes + 1
     return (t)
