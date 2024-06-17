a0183ca9807fae250326872ba6a26eb26da92831
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-17 12:17:32 +0100

    add: add the rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,3 +2,15 @@
 """
     Rotate 2D Matrix solution Module
 """
+
+
+def rotate_2d_matrix(matrix):
+    """rotate a matrix 90 degrees clockwise"""
+    n = len(matrix)
+    for row in range(n):
+        if row < (n // 2):
+            for col in range((row + 1), n):
+                a = matrix[row][col]
+                matrix[row][col] = matrix[col][row]
+                matrix[col][row] = a
+        matrix[row] = matrix[row][::-1]
