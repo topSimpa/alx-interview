cd0b6e1f51901a1b22acb5ea423ebbc6f38d498b
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-06-29 16:54:22 +0100

    add: add 0-island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """island_perimeter"""
+    hold_col = 0
+    count = 0
+
+    for row in range(1, len(grid) - 1):
+        col = hold_col
+        while (col < len(grid)):
+            if ((grid[row][col] == 1) and (hold_col == 0)):
+                hold_col = col
+            if (grid[row][col] == 1):
+                count += 1
+            elif ((hold_col != 0) and (grid[row][col] == 0)):
+                break
+            col += 1
+    result = (count - 2) * 2 + 6
+    return (result)
