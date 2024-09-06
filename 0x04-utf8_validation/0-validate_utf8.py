afc07d8c13b28fac57af88865a0a2b17f4cb289c
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-09-06 01:05:31 +0100

    add: added 0-validate_utf8/py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,80 +1,51 @@
 #!/usr/bin/python3
+"""Utf8-Validation  Module
 """
-Main file for utf8_validation
-"""
 
 
-def xtract(num):
-    # print(f'integer {num}')
-    byte_a = bin(num)[2:]
-    # print(f'Extracting from {byte_a}')
-    if (len(byte_a) < 8):
-        byte_a = ((8 - len(byte_a)) * '0') + byte_a
-        # print(f'changed to {byte_a}')
-    bits = ''
-    for i in byte_a:
-        if i == '0':
-            bits += i
-            break
-        bits += i
-    # print(bits)
-    return (bits)
+def bytes_counter(num):
+    """
+    count the number of 1 which interprete to num_bytes of first member of data
+    """
+    shift_val = num >> 3
+    if shift_val >= 0 and shift_val <= 15:
+        return 0
+    elif shift_val >= 16 and shift_val <= 23:
+        return 1
+    elif shift_val >= 24 and shift_val <= 27:
+        return 2
+    elif shift_val >= 28 and shift_val <= 29:
+        return 3
+    elif shift_val == 30:
+        return 4
+    return None
 
 
-def is_byte(num):
-    return (num < 128)
+def valid_sequence(chars):
+    """to check if a byte start with 10
+    """
+    for char in chars:
+        if bytes_counter(char) != 1:
+            return False
+    return True
 
 
-def many_bytes(num):
-    # print(num)
-    a_byte = bin(num)[2:]
-    # print(a_byte)
-    if (len(a_byte) < 8):
-        a_byte = ((8 - len(a_byte)) * '0') + a_byte
-        # print(a_byte)
-    #elif len(a_byte) > 8:
-        #return (-1)
-    n_bytes = 0
-    for i in a_byte:
-        if i == '0':
-            break
-        n_bytes += 1
-    # print(n_bytes)
-    return (n_bytes)
-
-
-def right_shift(num):
-    # print((num >> 1) - 1)
-    return ((num >> 1) - 1)
-
-
-def validUTF8(data):
-    i = 0
-    t = False
-    while (i < len(data)):
-        if is_byte(data[i]):
-            t = True
-            i += 1
+def utf8_validation(data):
+    """Utf8-Validation
+       -Validate if coding is in utf8-format
+    """
+    cur_position = 0
+    while cur_position < len(data):
+        n_bytes = bytes_counter(data[cur_position])
+        if n_bytes == 0:
+            cur_position += 1
             continue
-        else:
-            n_bytes = many_bytes(data[i])
-            if (n_bytes > 3) or (n_bytes == -1):
-                return (False)
-            # print(f"i am {n_bytes} bytes")
-            # print("tried")
-            if (i + n_bytes + 1) > len(data):
-                # print('short')
-                return (False)
-            con_num = data[i: (i + n_bytes + 1)]
-            j = 0
-            while (j < len(con_num) - 1):
-                j_cur = int(xtract(con_num[j]), 2)
-                j_next = int(xtract(con_num[j + 1]), 2)
-                # print(j_cur, j_next)
-                if right_shift(j_cur) != j_next:
-                    # print('shifting error')
-                    return (False)
-                j += 1
-                t = True
-            i = i + n_bytes + 1
-    return (t)
+        elif n_bytes == 1:
+            return False
+        elif n_bytes is None:
+            return False
+        if not valid_sequence(data[cur_position + 1: cur_position + n_bytes]):
+            return False
+        cur_position = cur_position + n_bytes
+
+    return True
