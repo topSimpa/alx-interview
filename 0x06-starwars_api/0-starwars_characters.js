81cbf3c000c8c6e464ebbeb56a1ebd11bf71f855
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-11-19 06:16:14 +0100

    update 0-starswars_api: corrected styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = "https://swapi-api.alx-tools.com/api/films/" + id;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
 request.get(url, { json: true }, (err, resp, body) => {
   if (err) {
